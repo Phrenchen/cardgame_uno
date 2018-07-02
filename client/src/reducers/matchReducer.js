0e0d81141056c05fe96d57511089e079707bc7bf
Kevin Stähler <phrenchen@gmail.com>
2018-07-03 01:07:17 +0200

    distributing cards with effects to player. using redux store nice

--- client/src/reducers/matchReducer.js
+++ client/src/reducers/matchReducer.js
@@ -8,15 +8,10 @@ const initialState = {
 export default function(state = initialState, action){
     switch(action.type){
         case START_MATCH:
-            console.log(action.payload);
-            console.log(action.payload.cards);
-            console.log(action.payload.cards.length);
-            console.log(action.payload.cards[0]);
-            console.log(action.payload.cards[0].cardDeck);
             return {
                 players: action.payload.players,
                 //cards: action.payload.cards
-                cards: action.payload.cards[0].cardDeck         // weird parsing shit. clean up somewhere...server side? :/
+                cards: action.payload.cards         // weird parsing shit. clean up somewhere...server side? :/
             };
         default:
             return state;
--- routes/api/matches.js
+++ routes/api/matches.js
@@ -2,6 +2,8 @@ const uuid = require("uuid");
 const express = require("express");
 const matchRouter = express.Router();
 const Match = require("../../model/Match")
+const Player = require("../../model/Player")
+const Card = require("../../model/Card")
 const cardDeck = require("../../model/InitDB");
 
 matchRouter.get("/", (req, res) => {
@@ -18,51 +20,74 @@ matchRouter.get("/", (req, res) => {
         .then(players => res.json(players))
 });
 
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 matchRouter.post("/", (req, res) => {
     console.log("create new match");
-    console.log(req.body);
-    console.log(cardDeck);
-    const match = new Match({
-        id: uuid(),
-        players: req.body,
-        cards: cardDeck
-    });
-    //match.save...
-    match.save()
-        .then((pMatch) => {
-            res.json(pMatch);
-        });
+    //console.log(req.body);
+    //console.log(cardDeck);
+    
+    let playerIDs = req.body;
+    let players = [];   // PlayerObjects. serialize? hmm
+    let cards = [];
 
-    /*let joinMatch = null;
-
-    Match.find()
-        .then((matches) => {
-            console.log("found match for player id: " + req.body.playerID);
-            console.log("has cards: " + cardDeck.length);
-            matches.map((match) => {
-                if(joinMatch == null && match.players.length < MAX_PLAYER_COUNT){
-                    joinMatch = match;
-                    joinMatch.players.push(req.body.playerID);
-                    res.json(match);
+    Player.find()
+        .then((allPlayers) =>{
+            // find playerObjects matching to client request
+            allPlayers.map((player) =>{
+                if(playerIDs.indexOf(player.id) != -1){     // found one of the players
+                    players.push(player);
                 }
-            })
-
-            if(!joinMatch){
-                // create new match
-                match = new Match({
-                    id: uuid(),
-                    players: [],
-                    cards: cardDeck
-                });
-                match.players.push(req.body.playerID);
-                //match.save...
-                match.save()
-                    .then((pMatch) => {
-                        res.json(pMatch);
+            });
+        })
+        .then(() => {
+            Card.find()
+                .then((allCards) => {
+                    // distribute cards to players
+                    // for each player
+                    let playerCardCount = 7;
+                    let index;
+                    
+        
+                    players.map((player) =>{
+                        for(let i=0; i<playerCardCount; i++){
+                            index = getRandomInt(0, cardDeck.cardDeck.length-1);
+                            let cardID = cardDeck.cardDeck.splice(index, 1);
+                            let card = getCardByID(allCards, cardID);
+                            player.cards.push( card );  // move from deck to player hand
+                        }
                     });
-            }
-        });
-    */
+                    
+                    let stackCards = [];
+                    console.log(cardDeck.cardDeck.length);
+                    cardDeck.cardDeck.map((cardID) => {
+                        stackCards.push(getCardByID(allCards, cardID));
+                    });
+                    console.log(stackCards);
+                    const match = new Match({
+                        id: uuid(),
+                        players: players,
+                        cards: stackCards
+                    });
+                    //match.save...
+                    match.save()
+                        .then((pMatch) => {
+                            res.json(pMatch);
+                        });
+                })
+        })
 });
+
+function getCardByID(allCards, cardID){
+    let result = null;
+    allCards.map((card) => {
+        if(card.id == cardID){
+            result = card;
+        }
+    });
+    return result;
+}
 
 module.exports = matchRouter;
