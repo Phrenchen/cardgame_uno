8b07cd5796ed18652b8ce2b9166f6f0255addddd
Kevin Stähler <phrenchen@gmail.com>
2018-07-10 14:37:52 +0200

    added comment

--- client/src/store.js
+++ client/src/store.js
@@ -8,7 +8,7 @@ const store = createStore(roodReducer,
     initialState, 
     compose(
         applyMiddleware(...middleware)//,
-        //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()      // enable for debugging
     ));
 
 export default store;
