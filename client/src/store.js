a5600fe7bf1333e83ffdb727d0a76b88c276254e
Kevin Stähler <phrenchen@gmail.com>
2018-07-10 13:15:01 +0200

    removed devtool extensions from store. that fixed an early error in non-chrome browsers. so glad ^^

--- client/src/store.js
+++ client/src/store.js
@@ -7,8 +7,8 @@ const middleware = [thunk];
 const store = createStore(roodReducer, 
     initialState, 
     compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        applyMiddleware(...middleware)//,
+        //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     ));
 
 export default store;
