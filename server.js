29b0800872da169c4167530e3a7c0a086c3f7d3e
Kevin Stähler <phrenchen@gmail.com>
2018-07-17 01:42:12 +0200

    Revert "changed call order of db init call...if things go badly wrong, revert this one. good luck, future me"
    This reverts commit d21cb591ace668ea244547d42403bfcbd7b6185c.

--- server.js
+++ server.js
@@ -13,16 +13,13 @@ app.use(bodyParser.json());
 const db = require("./config/keys").mongoURI;
 
 //mongoose.connection.dropCollection();
+mongoose.connection.dropDatabase(); // <---- DROP DATA!
 
 // connect to db
 mongoose
-.connect(db)
-.then(() => {
-    console.log("mongoDB connected")
-        mongoose.connection.dropDatabase(); // <---- DROP DATA!
-        initDB();
-    })
-    .catch((e) => console.log(e))
+    .connect(db)
+    .then(() => console.log("mongoDB connected") )
+    .catch(() => console.log("err"))
 
 
 // use route
@@ -41,3 +38,5 @@ if(process.env.NODE_ENV === "production"){
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log("server started on port " + port));
+
+initDB();
